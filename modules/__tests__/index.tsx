e867680a1e1fd40e9a244c559495a5c74de2dfee
Joe McGrath <joe.mcgrath@fanduel.com>
2018-05-31 17:52:46 +0100

    Tests for new API

--- modules/__tests__/index.tsx
+++ modules/__tests__/index.tsx
@@ -4,21 +4,53 @@ import { propsToContext, contextToProps } from '../'
 
 interface P {
     name?: string
+    surname?: string
 }
 
 describe('react-zap', () => {
     describe('propsToContext', () => {
-        it('should set props to context', () => {
-            const { Consumer, Provider } = React.createContext<P>({})
-            const consumer = jest.fn().mockReturnValue(<div />)
-            const BaseComponent = () => <Consumer>{consumer}</Consumer>
-            const Component = propsToContext<P>(Provider, {
-                include: ['name'],
-                exclude: []
-            })(BaseComponent)
-            const output = render(<Component name="foo" />)
+        describe('when no config is passed', () => {
+            it('should set all props to context', () => {
+                const { Consumer, Provider } = React.createContext<P>({})
+                const consumer = jest.fn().mockReturnValue(<div />)
+                const BaseComponent = () => <Consumer>{consumer}</Consumer>
+                const Component = propsToContext<P>(Provider)(BaseComponent)
+                const output = render(<Component name="foo" surname="bar" />)
 
-            expect(consumer).toHaveBeenCalledWith({ name: 'foo' })
+                expect(consumer).toHaveBeenCalledWith({
+                    name: 'foo',
+                    surname: 'bar'
+                })
+            })
+        })
+
+        describe('when an array is passed as config', () => {
+            it('should set only the defined props to context', () => {
+                const { Consumer, Provider } = React.createContext<P>({})
+                const consumer = jest.fn().mockReturnValue(<div />)
+                const BaseComponent = () => <Consumer>{consumer}</Consumer>
+                const Component = propsToContext<P>(Provider, ['name'])(
+                    BaseComponent
+                )
+                const output = render(<Component name="foo" surname="bar" />)
+
+                expect(consumer).toHaveBeenCalledWith({ name: 'foo' })
+            })
+        })
+
+        describe('when a function is passed as config', () => {
+            it('should use the custom map function', () => {
+                const { Consumer, Provider } = React.createContext<P>({})
+                const consumer = jest.fn().mockReturnValue(<div />)
+                const BaseComponent = () => <Consumer>{consumer}</Consumer>
+                const Component = propsToContext<P, {}>(
+                    Provider,
+                    ({ name, ...props }) => props
+                )(BaseComponent)
+                const output = render(<Component name="foo" surname="bar" />)
+
+                expect(consumer).toHaveBeenCalledWith({ surname: 'bar' })
+            })
         })
     })
 
