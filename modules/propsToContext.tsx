7d817d1bfb843113a5b13b1d640cb065509413ab
Thomas Roch <thomas.c.roch@gmail.com>
2018-06-04 12:18:39 +0100

    refactor: update type definitions and satisfy linting

--- modules/propsToContext.tsx
+++ modules/propsToContext.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react'
 
-function createPropsToProvider<P>(include: string[]): (props: P) => Partial<P> {
-    return function (props: P): Partial<P> {
-        return filterProps<P>(props, propName => include.indexOf(propName) !== -1)
-    }
+function createPropsToProvider<P, C>(include: string[]): (props: P) => C {
+    return (props: P): C =>
+        filterProps<P, C>(props, propName => include.indexOf(propName) !== -1)
 }
 
-function filterProps<P>(
+function filterProps<P, C>(
     props: P,
     predicate: (propName: string) => boolean
-): Partial<P> {
-    return Object.keys(props).reduce((p: Partial<P>, propName: string) => {
+): C {
+    return Object.keys(props).reduce((p: Partial<C>, propName: string) => {
         if (predicate(propName)) {
             p[propName] = props[propName]
         }
 
         return p
-    }, {})
+    }, {}) as C
 }
 
 export default function propsToContext<P, C extends P = P>(
@@ -25,27 +24,21 @@ export default function propsToContext<P, C extends P = P>(
 ) {
     return (BaseComponent: React.ComponentType<P>): React.ComponentClass<P> =>
         class PropsToContext extends React.Component<P> {
-            public propsToProvider = props => props
-
             constructor(props) {
                 super(props)
 
                 if (typeof config === 'function') {
                     this.propsToProvider = config
                 } else if (config) {
-                    this.propsToProvider = createPropsToProvider(config)
+                    this.propsToProvider = createPropsToProvider<P, C>(config)
                 }
             }
 
+            public propsToProvider = props => props
+
             public render() {
                 return (
-                    <Provider
-                        value={
-                            this.propsToProvider(
-                                this.props,
-                            ) as C
-                        }
-                    >
+                    <Provider value={this.propsToProvider(this.props)}>
                         <BaseComponent {...this.props} />
                     </Provider>
                 )
